30bb5242c874fb889b513e7eade1b62a1323962b
ecl01 <akf0g7@gmail.com>
2011-11-20 18:44:31 +0900

    000

---
+++ src/com/gittest2/GitTest2Activity.java
@@ -0,0 +1,19 @@
+package com.gittest2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GitTest2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //編集しました
+        //また編集
+        //asdghj
+        //2222
+        //2222
+        //2222
+        //2222
+    }
+}
