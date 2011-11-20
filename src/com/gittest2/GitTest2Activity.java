9e16f60e262237c56d3bac0ca459d525f729af05
ecl01 <akf0g7@gmail.com>
2011-11-20 16:17:28 +0900

    コミット1

---
+++ src/com/gittest2/GitTest2Activity.java
@@ -0,0 +1,13 @@
+package com.gittest2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GitTest2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
